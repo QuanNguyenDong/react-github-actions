1cfb2d1aa79a1b4dbd87e263b4d877c6c711a7a3
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2023-06-17 20:31:11 +1000

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +19,29 @@ function App() {
           Learn React
         </a>
         <p>Hello world</p>
-      </header>
+      </header> */}
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <Route exact={true} path="/dashboard" component={DashboardPage} />
+      </Switch>
     </div>
   );
+}
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  )
 }
 
 export default App;
